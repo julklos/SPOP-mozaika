1ca5e8ebcc921c4f3e7a787bfb477e59fec68cd1
Julia <julia.klos159@gmail.com>
2020-12-19 17:05:36 +0100

    reading files + one sample mosaic

--- mosaic/src/Main.hs
+++ mosaic/src/Main.hs
@@ -1,5 +1,19 @@
 module Main where
+import System.IO (readFile)
 
+-- pobierz łamigłówkę z pliku
+readPuzzle :: String -> IO [String]
+readPuzzle filename = do
+  contents <- readFile filename
+  let puzzle = read contents :: [String]
+  return puzzle
+-- wprowadź ścieżkę do pliku, z którego ma zostać pobrana łamigłówka
+getFileName :: IO String
+getFileName = do putStrLn "Podaj nazwę pliku, z którgo ma zostać pobrana łamigłówka:"
+                 filename <- getLine --użytkownik wprowadza nazwę
+                 return filename
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = do putStrLn "Mosaic"
+          filename <- getFileName
+          puzzle <- readPuzzle filename
+          print (show $ puzzle)
