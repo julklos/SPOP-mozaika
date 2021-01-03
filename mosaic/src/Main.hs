1571153f5877a88ca7d4719d4fdb42a43dfd3083
Julia <julia.klos159@gmail.com>
2021-01-03 14:12:26 +0100

    color cell

--- mosaic/src/Main.hs
+++ mosaic/src/Main.hs
@@ -40,8 +40,9 @@ v :: Value
 v = Just 5
 
 table :: Table
-table = [[C UNDECIDED (Just 5), C UNDECIDED Nothing ],
-        [C UNDECIDED Nothing , C UNDECIDED Nothing ]]
+table = [[C UNDECIDED (Just 3), C WHITE Nothing, C UNDECIDED Nothing ],
+        [C UNDECIDED Nothing , C BLACK (Just 4) ,C UNDECIDED Nothing],
+        [C UNDECIDED Nothing , C UNDECIDED Nothing , C BLACK Nothing]]
 -- table = [[(UNDECIDED, Nothing),(UNDECIDED, Nothing), (UNDECIDED,(Just 5)),(UNDECIDED, Nothing),(UNDECIDED, Nothing),(UNDECIDED, Nothing),(UNDECIDED, Nothing),(UNDECIDED,5), (UNDECIDED,4), (UNDECIDED,Nothing)],
 --                 [(UNDECIDED,Nothing),(UNDECIDED,5), (UNDECIDED, Nothing),(UNDECIDED, Nothing),(UNDECIDED,6),(UNDECIDED, Nothing),(UNDECIDED, Nothing),(UNDECIDED,5),(UNDECIDED, Nothing),(UNDECIDED, Nothing)],
 --                 [(UNDECIDED,4), (UNDECIDED,Nothing ), (UNDECIDED,2), (UNDECIDED,Nothing), (UNDECIDED,5),(UNDECIDED, Nothing),(UNDECIDED, Nothing),(UNDECIDED, Nothing),(UNDECIDED,4),(UNDECIDED,4)],
@@ -66,7 +67,40 @@ table = [[C UNDECIDED (Just 5), C UNDECIDED Nothing ],
 -- setValue x row = case x of
 --                     "\""
 -- convertToTable (x:xs) row  = setValue x row : convertToTable xs row
- 
+colourCells :: State -> [(Int, Int)] -> Table -> Table
+colourCells _ [] table = table
+colourCells color ((x,y):xs) table = let state = getState (byInd table x y)
+                                     in if state /= UNDECIDED then colourCells color xs table
+                                        else let val = getValue (byInd table x y)
+                                                 modifiedTable = replace_elem table x y (C color val)
+                                             in colourCells color xs modifiedTable
+
+checkIfToColour :: Table -> Int -> Int -> Table
+checkIfToColour table row col = let neighbourhoods = neighbourhoodsList row col table
+                                    states = stateList neighbourhoods table
+                                    whites = countState states WHITE
+                                    blacks = countState states BLACK
+                                    unds = countState states UNDECIDED
+                                    val = fromJust (getValue (byInd table row col))
+                                    not_val = length states - val
+                                  in if val - blacks == unds then colourCells BLACK neighbourhoods table
+                                     else if not_val - whites == unds then colourCells WHITE neighbourhoods table
+                                          else table
+                                    
+                                
+
+countState :: Eq a => [a] -> a -> Int
+countState [] find = 0
+countState (x:xs) find 
+  | find == x = 1 + (countState xs find)
+  | otherwise = countState xs find
+
+stateList :: [(Int, Int)] -> Table -> [State]
+stateList [] _           = []
+stateList ((x,y):xs) table = getState (byInd table x y): stateList xs table
+-- wygeneruj liste sąsiadów komórki
+neighbourhoodsList :: Int -> Int -> Table -> [(Int,Int)]
+neighbourhoodsList x y table = [ (x+dx,y+dy) | dy <- [-1..1], dx <- [-1..1], x+dx>=0, y+dy>=0, x+dx<(length table), y+dy<(length (table !! 0))]
 -- pobierz łamigłówkę z pliku
 readPuzzle :: String -> IO [String]
 readPuzzle filename = do
@@ -85,12 +119,26 @@ main :: IO ()
 main = do putStrLn "Mosaic"
           -- filename <- getFileName
           -- puzzle <- readPuzzle filename
-          -- let tablePuzzle = convertToTable puzzle 
-          let val =  table !! 1 !! 0
-          print (getValue val)
-          print( byInd table 1 0)
-          let newtable = replace_elem table 1 0 (C UNDECIDED (Just 3))
-          print (byInd table 1 0)
-          print (byInd newtable 1 0)
+          -- let tablePuzzle = convertToTable puzzle
+          print table
+          let newtable = checkIfToColour table 0 0
+          print newtable
+          -- let cols = length (table !! 0)
+          -- let rows = length table
+          -- let val =  table !! 1 !! 0
+          -- print (getValue val)
+          -- print( byInd table 1 0)
+          -- let newtable = replace_elem table 1 0 (C UNDECIDED (Just 3))
+          -- print (byInd table 1 0)
+          -- print (byInd newtable 1 0)
+          -- print (rows, cols)
+          -- let neighbourhoods = neighbourhoodsList 1 1 table
+          -- let states = stateList neighbourhoods table
+          -- let whites = countState states WHITE
+          -- let blacks = countState states BLACK
+          -- let unds = countState states UNDECIDED
+          -- let val = fromJust (getValue (byInd table 1 1))
+          -- let not_val = length states - val
+          -- print (val, not_val, length states)
           --solvedPuzzle <- solve tablePuzzle
           -- print puzzle
